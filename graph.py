e43d939f29d56b31ebe582edc7a0b6b8d991f7f4
Gazala939 <Gazalajamadar786@gmail.com>
2026-03-30 14:45:55 +0530

    Displaying Vertices

--- graph.py
+++ graph.py
@@ -1,8 +1,25 @@
-graph = {
+graph_structure = {
     'a':['b','c'],
     'b':['a','d'],
     'c':['a','d'],
     'd':['b','e'],
     'e':['d']
 }
-print(graph)
+print(graph_structure)
+
+# Displaying the Vertices:
+
+# Creating a class of graph
+class graph:
+    def __init__(self, gdict = None):
+        if gdict is None:
+            gdict = []
+        self.gdict = gdict
+
+    def getVertices(self):
+        return list(self.gdict.keys())
+
+
+    
+gobj = graph(graph_structure)
+print("The given Vertices are",gobj.getVertices())
